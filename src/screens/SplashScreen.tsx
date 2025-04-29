e414ee53c2f51ef2627fe88dd07a29dd6c03a37c
Jessica Woytuski <woytuski2@gmail.com>
2025-04-29 15:15:54 -0300

    feat: splash screen

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { COLORS } from '../utils/constants';
+
+export default function SplashScreen() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoWrapper}>
+                <Text style={styles.logoText}>TASKLY</Text>
+                <View style={styles.dot} />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.background,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logoWrapper: {
+        position: 'relative',
+        width: 169,
+        height: 56,
+        justifyContent: 'center',
+    },
+    logoText: {
+        fontFamily: 'Roboto',
+        fontWeight: '700',
+        fontSize: 48,
+        color: COLORS.mainText,
+    },
+    dot: {
+        position: 'absolute',
+        width: 19.5,
+        height: 19.5,
+        borderRadius: 9.75,
+        backgroundColor: COLORS.primaryLight,
+        top: 2.25,
+        right: -22,
+    },
+});
---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const COLORS = {
+    primary: '#3E20CF',
+    primaryLight: '#583CC4',
+    background: '#F4F4F4',
+    mainText: '#1E1E1E',
+};
